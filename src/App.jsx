61b47f25466f60580fdb2a5e998da5a0a725d6b4
miaduedam <duedamm@gmail.com>
2025-12-19 13:14:08 +0100

    WIP: login + security updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import Header from "./components/header/Header.jsx";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+import LogIn from "./components/security/LogIn.jsx"
+import facade from "./apiFacade.js";
+import LoggedIn from "./components/security/LoggedIn.jsx";
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [roles, setUserRoles] = useState('');
+  const [username, setUserName] = useState('');
+
+  const login = (user, pass) => {
+    facade.login(user,pass)
+    .then(() =>setLoggedIn(true));
+    const [username, roles] = facade.getUsernameAndRoles();
+    setUserRoles(roles);
+    setUserName(username);
+    
+  }
+
+   const logout = () => {
+    setLoggedIn(false);
+    facade.logout();
+  }
   const headers = [
     { title: "Home", url: "/" },
     { title: "Who Said it", url: "/WhoSaidIt" },
@@ -11,11 +33,20 @@ function App() {
     { title: "API Documentation", url: "/APIDocumentation" }
   ];
 
-  
   return (
     <>
       <Header headers={headers} />
       <Outlet />
+
+      <div>
+      {!loggedIn ? (<LogIn login={login} />) :
+        (<div>
+          <LoggedIn loggedIn={loggedIn} />
+          <button onClick={logout}>Logout</button>
+          <h2>Username: {username}</h2>
+          <p>{roles}</p>
+        </div>)}
+    </div>
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,13 +19,6 @@ const Header = ({ headers }) => {
           </NavLink>
         ))}
       </nav>
-
-      {/* Temporary login placeholder */}
-      <div className={styles.loginPlaceholder}>
-        <input type="text" placeholder="Username" className={styles.input} />
-        <input type="password" placeholder="Password" className={styles.input} />
-        <button className={styles.button}>Login</button>
-      </div>
     </header>
   );
 };
