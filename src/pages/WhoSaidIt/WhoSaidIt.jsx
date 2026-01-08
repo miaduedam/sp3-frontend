ff7671149b8f052ec39488245b5ef140c4839367
miaduedam <duedamm@gmail.com>
2026-01-08 13:40:54 +0100

    just a change in css all quotes, and some boiler code

--- src/pages/WhoSaidIt/WhoSaidIt.jsx
+++ src/pages/WhoSaidIt/WhoSaidIt.jsx
@@ -12,8 +12,6 @@ export default function QuoteGame() {
   const [highScores, setHighScores] = useState([]);
 
   useEffect(() => {
-    if (!facade.loggedIn()) return;
-
     facade
       .fetchData("quotes")
       .then((data) => {
